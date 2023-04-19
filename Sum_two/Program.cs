bc592e30b8cd013dca5b19052e79b096d09f1c2f
Ivan <trubnikov_is@gkl-kemerovo.ru>
2023-04-19 16:16:04 +0300

    made random numbers

--- Sum_two/Program.cs
+++ Sum_two/Program.cs
@@ -1,4 +1,4 @@
-﻿int numberA = 3;
-int numberB = 10012;
+﻿int numberA = new Random().Next(1; 19); // 1 2 3 4 ... 18
+int numberB = new Random().Next(1; 19);
 int result = numberA + numberB;
 Console.WriteLine(result);
