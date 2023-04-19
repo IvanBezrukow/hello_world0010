001f1525aeea8785c1bd804cf4db4789dd43ff2a
Ivan <trubnikov_is@gkl-kemerovo.ru>
2023-04-19 22:07:58 +0300

    2 friends and dog speed

---
+++ dog_count/Program.cs
@@ -0,0 +1,24 @@
+﻿int distance = 10000; 
+int time = 0;
+int count = 0;
+int FirstFriendSpeed = 1;
+int SecondFriendSpeed = 2;
+int DogSpeed = 5;  
+
+while (distance > 10)
+{
+    int friend = 2;
+    if (friend == 2)
+    {
+        time = distance / (FirstFriendSpeed + DogSpeed);
+        friend = 2;
+    }
+    else time = distance / (SecondFriendSpeed + DogSpeed);
+    friend = 1;
+ 
+ distance = distance - (FirstFriendSpeed + SecondFriendSpeed) * time;
+ count = count + 1;
+} 
+Console.Write("собака пробежала ");
+Console.WriteLine(count); 
+Console.WriteLine("раз");
