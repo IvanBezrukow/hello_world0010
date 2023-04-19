8cc832eaa9918d59f2df890f9c527d40058ec743
Ivan <trubnikov_is@gkl-kemerovo.ru>
2023-04-19 16:22:00 +0300

    fixed the built

--- Sum_two/Program.cs
+++ Sum_two/Program.cs
@@ -1,4 +1,4 @@
-﻿int numberA = new Random().Next(1; 19); // 1 2 3 4 ... 18
-int numberB = new Random().Next(1; 19);
+﻿int numberA = new Random().Next(1, 19); // 1 2 3 4 ... 18
+int numberB = new Random().Next(1, 19);
 int result = numberA + numberB;
 Console.WriteLine(result);
