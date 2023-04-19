545ba7c20565bc83438ea1481007a6edaf41f7c2
Ivan <trubnikov_is@gkl-kemerovo.ru>
2023-04-19 17:17:49 +0300

    added random numebrs and 2 lines

--- Sum_two/Program.cs
+++ Sum_two/Program.cs
@@ -1,4 +1,6 @@
 ﻿int numberA = new Random().Next(1, 19); // 1 2 3 4 ... 18
+Console.WriteLine(numberA);
 int numberB = new Random().Next(1, 19);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
