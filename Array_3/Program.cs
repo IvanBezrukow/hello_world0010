855d69d6ad8544199b39ebdb65f12ea636744092
Ivan <trubnikov_is@gkl-kemerovo.ru>
2023-04-24 04:22:01 +0300

    completed Array_3

---
+++ Array_3/Program.cs
@@ -0,0 +1,44 @@
+﻿void FillUpArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintOutArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+int[] fuck = new int[10];
+FillUpArray(fuck);
+fuck[4] = 4;
+fuck[6] = 4;
+PrintOutArray(fuck);
+Console.WriteLine();
+int pos = IndexOf(fuck, 4);
+Console.WriteLine(pos);
